3723d089967d7b9ea55e11670c9730cc89717022
NetSnake <netsnakecn@163.com>
2019-08-02 11:22:08 +0800

    Fix bug for data source switcher

--- common/src/com/maicard/core/ds/DataSourceSwitcherAdapter.java
+++ common/src/com/maicard/core/ds/DataSourceSwitcherAdapter.java
@@ -14,23 +14,31 @@ import org.slf4j.LoggerFactory;
  */
 public class DataSourceSwitcherAdapter extends MethodHandlerAspectInterceptorAdapter {
 
-    protected final Logger logger = LoggerFactory.getLogger(this.getClass());
+	protected final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    @Override
-    public Object doAround(ProceedingJoinPoint jp) throws Throwable {
+	static final String defaultSource = "default";
 
-        Class<?> target=jp.getTarget().getClass();
-        try {
-            DataSourceMapper dataSourceMapper=target.getAnnotation(DataSourceMapper.class);
-            if(null!=dataSourceMapper && !StringUtils.isEmpty(dataSourceMapper.value())){
-                DataSourceSwitcher.setDataSourceType(dataSourceMapper.value());
-                logger.debug("Switching DataSource ["+target.getName()+", type=" +(StringUtils.isEmpty(DataSourceSwitcher.getDataSourceType())?"default":dataSourceMapper.value())+"]");
-            }
-            return jp.proceed();
-        } finally{//执行完成后将数据源标示清楚回复默认数据源
-        	
-            DataSourceSwitcher.clear();
-            logger.debug("Cleaned DataSource ["+target.getName()+", type=" +(StringUtils.isEmpty(DataSourceSwitcher.getDataSourceType())?"default":DataSourceSwitcher.getDataSourceType())+"]");
-        }
-    }
+	@Override
+	public Object doAround(ProceedingJoinPoint jp) throws Throwable {
+
+		Class<?> target=jp.getTarget().getClass();
+		try {
+			DataSourceMapper dataSourceMapper=target.getAnnotation(DataSourceMapper.class);
+			if(null!=dataSourceMapper && !StringUtils.isEmpty(dataSourceMapper.value())){
+				DataSourceSwitcher.setDataSourceType(dataSourceMapper.value());
+				logger.debug("Switching DataSource as annotation["+target.getName()+", type=" +(StringUtils.isEmpty(DataSourceSwitcher.getDataSourceType())? defaultSource :dataSourceMapper.value())+"]");
+			} else {
+				//否则应当执行到default
+				logger.debug("Switching DataSource to default without annotation["+target.getName()+", type=" + defaultSource);
+				DataSourceSwitcher.setDataSourceType(defaultSource);
+
+
+			}
+			return jp.proceed();
+		} finally{//执行完成后将数据源标示清楚回复默认数据源
+
+			DataSourceSwitcher.clear();
+			logger.debug("Cleaned DataSource ["+target.getName()+", type=" +(StringUtils.isEmpty(DataSourceSwitcher.getDataSourceType())?"default":DataSourceSwitcher.getDataSourceType())+"]");
+		}
+	}
 }
